828c448b744d7ec6748237ab05d81a003db12288
Raven <123331869+RavenCunningham@users.noreply.github.com>
2025-08-06 13:37:43 +0100

    Add mathematical functions
    Add functions: add(a, b), subtract(a, b), multiply(a, b), and divide(a, b)
    Add function operate(a, op, b)
    link script to index.html

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(a, op, b) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+    }
+}
+
+console.log(add(5, 5));
+console.log(subtract(15, 5));
+console.log(multiply(2, 5));
+console.log(divide(100, 10));
